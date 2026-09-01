a0bb5c32dc908c4592ee0c3a47043fb77aca944d
Alex Valenko <valenko.a.geo@gmail.com>
2026-09-01 20:42:32 +0300

    added static files

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    # Здесь вы можете добавить пути к вашим статическим файлам,
+    # если они располагаются вне приложений
+    BASE_DIR / 'static'
+]
 
 MAILERS = {
     'default': {
