0c714c375ac6cdd8d110ab991f408f4b3c5471b0
Alex Valenko <valenko.a.geo@gmail.com>
2026-09-14 17:20:11 +0300

    added page products with paginator

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, product_details, add_product
+from catalog.views import add_product, contacts, home, product_details, products
 
 app_name = CatalogConfig.name
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path("home/", home, name="home"),
     path("contacts/", contacts, name="contacts"),
     path("product_details/<int:pk>", product_details, name="product_details"),
-    path("add_product/", add_product, name="add_product")
+    path("add_product/", add_product, name="add_product"),
+    path("products/", products, name="products"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,18 @@
 from itertools import product
 
+from django.core.paginator import Paginator
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from .models import Product, Category
+from django.shortcuts import redirect, render
+
+from .models import Category, Product
 
 
 def home(request):
     """Контроллер для отображения домашней страницы - отображает последние 6 добавленных товаров"""
 
-    latest_products = Product.objects.order_by('created_at')[:6]
-    context = {"products" : latest_products}
-
-    if latest_products:
-        return render(request, "home.html", context=context)
+    latest_products = Product.objects.order_by("-created_at")[:6]
+    context = {"products": latest_products}
+    return render(request, "home.html", context=context)
 
 
 def contacts(request):
@@ -25,29 +25,42 @@ def contacts(request):
         return HttpResponse(f"Спасибо, {name}! сообщение получено.")
     return render(request, "contacts.html")
 
+
 def product_details(request, pk):
     """Контроллер для отображения подробной информации о товаре"""
     current_product = Product.objects.get(pk=pk)
-    context = {'current_product' : current_product}
-    return render(request, 'product_details.html', context=context)
+    context = {"current_product": current_product}
+    return render(request, "product_details.html", context=context)
+
 
 def add_product(request):
     """Контроллер для заполнения данных о новом товаре"""
     categories = Category.objects.all()
 
     if request.method == "POST":
-        # Создаём объект товара из данных формы
         Product.objects.create(
             prod_name=request.POST.get("prod_name"),
             description=request.POST.get("description"),
             category_id=request.POST.get("category"),
             price=request.POST.get("price"),
-            prod_image=request.FILES.get("prod_image"),  # файлы берём из request.FILES
+            prod_image=request.FILES.get("prod_image"),
         )
-        # После сохранения — возврат на главную
+        print(f"Товар {request.POST.get('prod_name')} успешно добавлен в базу")
         return redirect("/home/")
 
-    context = {'categories' : categories}
-    return render(request, 'add_product.html', context=context)
+    context = {"categories": categories}
+    return render(request, "add_product.html", context=context)
 
 
+def products(request):
+    """Контроллер для вывода страницы с товарами (весь каталог товаров)"""
+    all_products = Product.objects.all().order_by("category")
+
+    paginator = Paginator(all_products, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "page_obj": page_obj,
+    }
+    return render(request, "products.html", context=context)
